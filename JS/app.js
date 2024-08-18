edf55fd212ce5880fb903c1cf82033b6c470db5e
anibal <anibalcoding@gmail.com>
2024-08-18 10:23:57 -0500

    first commit

---
+++ JS/app.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const toggleButton = document.querySelector('.navbar__mobile-menu-toggle');
+    const mobileMenu = document.querySelector('.navbar__mobile-menu-items');
+
+
+    toggleButton.addEventListener('click', () => {
+        mobileMenu.classList.toggle('active');
+    });
+});
+
+
+window.addEventListener('scroll', ()=> {
+    const navbar = document.querySelector('.navbar');
+
+    if (window.scrollY > 0) {
+        navbar.classList.add('navbar--scroll')
+
+    }
+
+    else  {
+        navbar.classList.remove('navbar--scroll')
+    };
+});
